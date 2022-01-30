9ccfe5355e5bf4a4506d3c35869aa092be368f6e
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-30 11:00:49 +0530

    env var added.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,8 @@
+<script>
+	const API_KEY = import.meta.env.VITE_API_KEY;
+	//  `https://api.rawg.io/api/games?key=${API_KEY}&page=1&page_size=20`
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
