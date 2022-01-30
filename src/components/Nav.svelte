41e527f4ead3562e2e407fa73428f93d76528271
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-30 13:06:56 +0530

    Nav component added.

---
+++ src/components/Nav.svelte
@@ -0,0 +1,10 @@
+<nav class="max-w-5xl mx-auto py-3 px-6 flex items-center justify-between">
+	<h1 class="font-bold text-2xl">GameDB</h1>
+	<ul class="flex">
+		<li class="ml-6 font-medium text-base hover:text-purple-700"><a href="/">Home</a></li>
+		<li class="ml-6 font-medium text-base hover:text-purple-700"><a href="/about">About</a></li>
+		<li class="ml-6 font-medium text-base hover:text-purple-700">
+			<a href="https://github.com/Aditya6101/sveltekit-game-db">GitHub</a>
+		</li>
+	</ul>
+</nav>
